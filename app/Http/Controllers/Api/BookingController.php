4bcb3a6eae759ec833431f737f0a0acebfc6a588
laptopstore <mrqiu220220@uniq@edu.iq>
2026-06-22 19:01:24 +0300

    Refactor BookingController code smells

--- app/Http/Controllers/Api/BookingController.php
+++ app/Http/Controllers/Api/BookingController.php
@@ -10,8 +10,15 @@ class BookingController extends Controller
 {
     public function store(Request $request)
     {
+        $request->validate([
+            'hotel_id' => 'required',
+            'check_in' => 'required|date',
+            'check_out' => 'required|date',
+            'total_price' => 'required|numeric'
+        ]);
+
         $booking = Booking::create([
-            'user_id' => 1,
+            'user_id' => auth()->id(),
             'hotel_id' => $request->hotel_id,
             'check_in' => $request->check_in,
             'check_out' => $request->check_out,
